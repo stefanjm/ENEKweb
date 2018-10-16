7d609e90651b3533af1b87634f86c23535876105
stefan <stefan.joemagi@gmail.com>
2018-10-16 17:29:41 +0200

    Add project files.

---
+++ ENEKweb/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ENEKweb.Models;
+
+namespace ENEKweb.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Alternative Design
+        public IActionResult IndexOld() {
+            return View();
+        }
+
+        public IActionResult Tehtudtood()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Partnerid() {
+            return View();
+        }
+
+        public IActionResult Leiunurk() {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ENEKweb/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// Cycle through logo style when clicked on
+
+var logoCount = 1;
+$("#changeLogo").click(function () {
+    if (logoCount < 6) {
+        $("#logo_image").attr("src", "/images/logo/nr." + logoCount + ".png");
+        logoCount++;
+    }
+    else {
+        logoCount = 1;
+    }
+});
+
+$("#changeLogoFirst").click(function () {
+    if (logoCount < 6) {
+        $("#imas").css("background", "url(/images/logo/nr." + logoCount + ".png) no-repeat scroll");
+        $("#imas").css("background-position", "center");
+        logoCount++;
+    }
+    else {
+        logoCount = 1;
+    }
+});
