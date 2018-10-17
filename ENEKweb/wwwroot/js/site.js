367a806c563acca6cc6a5b99d729349b578dfd06
stefan <stefan.joemagi@gmail.com>
2018-10-18 00:32:27 +0200

    Leiunurk, added carousel gallery img to sellable items

--- ENEKweb/wwwroot/js/site.js
+++ ENEKweb/wwwroot/js/site.js
@@ -2,27 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.carousel .carousel-item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-// Cycle through logo style when clicked on
-
-var logoCount = 1;
-$("#changeLogo").click(function () {
-    if (logoCount < 6) {
-        $("#logo_image").attr("src", "/images/logo/nr." + logoCount + ".png");
-        logoCount++;
+    if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
     }
     else {
-        logoCount = 1;
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
+
+    
 });
 
-$("#changeLogoFirst").click(function () {
-    if (logoCount < 6) {
-        $("#imas").css("background", "url(/images/logo/nr." + logoCount + ".png) no-repeat scroll");
-        $("#imas").css("background-position", "center");
-        logoCount++;
-    }
-    else {
-        logoCount = 1;
-    }
+// Set active img as main img
+$('.carousel-control-next').click(function () {
+    $(this).closest('.mein-card').find('.mein-card-img').find('img').prop('src', $(this).closest('.carousel').children('.carousel-inner')
+        .children('.active').children('img').eq(2).prop('src'));
+
+});
+
+$('.carousel-control-prev').click(function () {
+    $(this).closest('.mein-card').find('.mein-card-img').find('img').prop('src', $(this).closest('.carousel').children('.carousel-inner')
+        .children('.active').children('img').eq(2).prop('src'));
+
 });
