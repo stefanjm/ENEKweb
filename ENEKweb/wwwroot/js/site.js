1b2f6bc0e950ec1f4bd8af8c60bd194307b1222b
stefan <stefan.joemagi@gmail.com>
2018-11-13 04:28:46 +0100

    New cards to display items

--- ENEKweb/wwwroot/js/site.js
+++ ENEKweb/wwwroot/js/site.js
@@ -2,40 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$('.carousel .carousel-item').each(function () {
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-
-   
-});
 
 
-// Set active img as main img
-$('.carousel-control-next').click(function () {
-    // main image
-    var mainImage = $(this).closest('.mein-card').find('.mein-card-img').find('img');
-    mainImage.prop('src', $(this).closest('.carousel').children('.carousel-inner')
-        .children('.active').children('img').prop('src'));
 
-});
-
-$('.carousel-control-prev').click(function () {
-    // main image
-    var mainImage = $(this).closest('.mein-card').find('.mein-card-img').find('img');
-    mainImage.prop('src', $(this).closest('.carousel').children('.carousel-inner')
-        .children('.active').children('img').prop('src'));
-
-});
-
-
-//sidebar
 $(document).ready(function () {
 
+    // sidebar
     $('#sidebarCollapse').on('click', function () {
         $('.sidebar').toggleClass('mobile-show');
+    });
+
+    // image slider
+    $('.img-next').on('click', function () {
+        var currentImg = $(this).closest("div").find("img.active");
+        var nextImg = currentImg.next("img");
+
+        if (nextImg.length) {
+            currentImg.removeClass('active');
+            nextImg.addClass('active');
+        }
+    });
+
+    $('.img-prev').on('click', function () {
+        var currentImg = $(this).closest("div").find("img.active");
+        var prevImg = currentImg.prev("img");
+
+        if (prevImg.length) {
+            currentImg.removeClass('active');
+            prevImg.addClass('active');
+        }
     });
 
 }); 
