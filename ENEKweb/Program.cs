c27ac9d87aa0db4d2a9eb7bf343557090e4fd5d1
stefan <stefan.joemagi@gmail.com>
2018-12-02 22:50:54 +0100

    On startup, runs the database initializer class with injected services.

--- ENEKweb/Program.cs
+++ ENEKweb/Program.cs
@@ -1,22 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
-using System.Threading.Tasks;
+using ENEKweb.Areas.Admin.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
+using IdentityData;
+using IdentityData.Models;
 
 namespace ENEKweb {
     public class Program {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            // Initialize the database, adding default admin user
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                try {
+                    var context = services.GetRequiredService<IdentityDataDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    DbInitializer.SeedUsers(context, userManager, roleManager);
+                }
+                catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
-        // Run specified startup config ( Development, Production, Default)
+        /// <summary>
+        /// Run specified startup configuration ( Development, Production, Default ) and specify port to run on
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
             var assemblyName = typeof(Startup).GetTypeInfo().Assembly.FullName;
 
