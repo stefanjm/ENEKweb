13a11b252883f83c78b8c4abd0c962a87a921121
stefan <stefan.joemagi@gmail.com>
2018-12-09 02:42:40 +0100

    Identity generates Npgsql Identity columns now

--- IdentityData/IdentityDataDbContext.cs
+++ IdentityData/IdentityDataDbContext.cs
@@ -5,7 +5,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace IdentityData {
     public class IdentityDataDbContext : IdentityDbContext<ApplicationUser> {
-        public IdentityDataDbContext(DbContextOptions<IdentityDataDbContext> options) : base(options) { }
+        public IdentityDataDbContext(DbContextOptions<IdentityDataDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ForNpgsqlUseIdentityColumns();
+        }
     }
 
 }
